17019c095d5b010c2b7c183c66d821f312861de8
Daniel González Briseño <sonusbeat@hotmail.com>
2013-12-23 19:04:14 -0600

    Se agregaron imagenes tamaño completo de carnes al menú

--- includes/platillos_array.php
+++ includes/platillos_array.php
@@ -74,6 +74,51 @@ $platillos = array(
 		"titulo" => "Fetuccini al Pesto con Pollo",
 		"img" => "pasta-fetuccini-al-pesto-pollo.jpg",
 		"des" => "Pasta fetuccini, salsa al pesto, tomate deshidratado, espinacas, pollo a la parrilla y queso parmesano."
-	)
+	),
+	"carne-filete-sammys" => array(
+		"titulo" => "Filete Sammys",
+		"img" => "carne-filete-sammys.jpg",
+		"des" => "300 gr con cama de espinacas bañado con salsa Sammy's y crocante de camote."
+	),
+	"carne-filete-chemita" => array(
+		"titulo" => "Filete Chemita",
+		"img" => "carne-filete-chemita.jpg",
+		"des" => "300gr con salsa de chile serrano, cebolla cambray, aceite de olica y salsa inglesa."
+	),
+	"carne-lomo-saltado" => array(
+		"titulo" => "Lomo Saltado",
+		"img" => "carne-lomo-saltado.jpg",
+		"des" => "tiras de filete de res, julianas de cebolla morada, julianas de jitomate, chicaros, papas, cilantro, ají amarillo, acompañado acompañado de arroz al vapor."
+	),
+	"carne-steack-diana" => array(
+		"titulo" => "Steack Diana",
+		"img" => "carne-steack-diana.jpg",
+		"des" => "450 gr con salsa de champiñones cremosa, con papa cambray y chicharos a la mantequilla."
+	),
+	"carne-ribeye-sammys" => array(
+		"titulo" => "Rib Eye Sammy's",
+		"img" => "carne-ribeye-sammys.jpg",
+		"des" => "450 gr con salsa Sammy's, acompañado con papa cambray y esparragos."
+	),
+	"carne-cowboy-roberto" => array(
+		"titulo" => "Cowboy Roberto",
+		"img" => "carne-cowboy-roberto.jpg",
+		"des" => "450 gr con cebollas, champiñones caramelizados, acompañados de papa cambray y ensalada."
+	),
+	"carne-arrachera" => array(
+		"titulo" => "Arrachera Importada",
+		"img" => "carne-arrachera.jpg",
+		"des" => "350 gr acompañada de frijoles refritos, guacamole, salsa martajada, nopales, chiles, cebollas y tomates asados con torillas recién hechas."
+	),
+	"carne-vacio-tradicional" => array(
+		"titulo" => "Vacio Tradicional",
+		"img" => "carne-vacio-tradicional.jpg",
+		"des" => "450 gr acompañado de papas cambray y ensalada."
+	),
+	"carne-vacio-florentino" => array(
+		"titulo" => "Vacio Florentino",
+		"img" => "carne-vacio-florentino.jpg",
+		"des" => "450 gr con espinacas a la crema, migas de panco al romero acompañado de papas cambray."
+	)	
 );
 ?>
