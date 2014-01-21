0fa986290dfe77dfc6afb0e63023e8cfe871d80c
Daniel González Briseño <sonusbeat@hotmail.com>
2014-01-20 21:01:32 -0600

    Se creó la galería de imágenes en miniatura de las instalaciones

---
+++ _components/galeria-imagenes-instalaciones.php
@@ -0,0 +1,60 @@
+<div class="photo-gallery">
+	<div class="index-list">
+		<div class="row">
+			<div class="index-item">
+				<a href="#"><img src="/images/instalaciones/miniaturas/entrada-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/buffete-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/vinos-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/interior-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+		</div><!-- END .row -->
+		<div class="row">
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/interior-clientes-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/seccion-interior-terrazas-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/interior-buffete-lateral-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/cantante-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+		</div><!-- END .row -->
+		<div class="row">
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/interior-terrazas-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/terraza-guadalupe-tacos-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/terraza-guadalupe-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+			<div class="index-item">
+				<a href="#"><img src="images/instalaciones/miniaturas/interior-vista-lateral-buffete-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+			</div><!-- END .index-item -->
+		</div><!-- END .row -->
+		<div class="pages">
+			<table>
+				<tr>
+					<td>
+						<!-- <a href="#" class="previous"><span>Anterior</span></a> -->
+						<a href="#" class="active"><span>1</span></a>
+						<a href="#"><span>2</span></a>
+						<a href="#" class="next"><span>Siguientes</span></a>
+					</td>
+				</tr>
+			</table>
+		</div><!-- END .pages -->
+	</div><!-- END .index-list -->
+</div><!-- END .photo-gallery -->
+<div class="clear"></div>
+<p class="show-all last"><a href="/"><span>Volver a Inicio</span></a></p>
---
+++ instalaciones-zazon-restaurant-bar.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+	<head>
+		<title>Zazón Restaurant Bar - ¿ Quienes Somos ?</title>
+		<!-- Meta Tags -->
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<!-- SEO -->
+		<meta name="robots" content="index, follow">
+		<meta name="description" content="Restaurant Bar de Cocina Internacional en Zapopan, Jalisco, México">
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+		<!-- Stylesheets -->
+		<link rel="stylesheet" href="css/sexy-slider.css" type="text/css" />
+		<link rel="stylesheet" href="css/content-slider.css" type="text/css" />
+		<link rel="stylesheet" href="css/main-stylesheet-min.css" type="text/css" />
+		<link type="text/css" id="style-responsive" rel="stylesheet" media="screen" href="css/responsive/desktop.css" />
+		<!--[if IE 7]>
+			<link rel="stylesheet" href="css/ie7.css" type="text/css" />
+		<![endif]-->
+
+		<!-- JavaScripts -->
+		<script src="js/jquery.min.js" type="text/javascript"></script>
+		<script src="js/jquery.sexyslider.js" type="text/javascript"></script>
+		<script src="js/jquery.contentslider.js" type="text/javascript"></script>
+		<script src="js/cufon-yui.js" type="text/javascript"></script>
+		<script src="js/museo.font.js" type="text/javascript"></script>
+		<script src="js/jquery.tinytips.js" type="text/javascript"></script>
+		<?php include('_components/about-us.js.html'); ?>
+	</head>
+	<body class="top">
+		<div class="container">
+			<div class="main-header-wrapper">
+				<header class="header">
+					<?php #include('_components/feedback.html'); ?>
+					<?php include('_components/logo-zazon.html'); ?>
+				</header>
+			</div><!-- END .main-header-wrapper -->
+			<?php include('_components/main-menu.html'); ?>
+			<div class="content-wrapper no-content-slider-wrapper">
+				<div class="content">
+					<div class="full-width-wrapper">
+						<table>
+							<tr>
+								<td class="full-width-content-wrapper">
+									<div class="top"></div>
+									<div class="content-wrapper">
+										<div class="content">
+											<div class="full-width-content">
+												<div class="main-title">
+													<span><b>Instalaciones</b></span>
+													<a href="/">Volver a Inicio</a>
+												</div>
+												<div class="full-width">
+													<h3>Galeria de Imagenes</h3>
+													<?php include('_components/galeria-imagenes-instalaciones.php'); ?>
+												</div><!-- END .full-width-content -->
+											</div><!-- END .full-width-content -->
+										</div><!-- END .content -->
+									</div><!-- END .content-wrapper -->
+								</td>
+							</tr>
+							<tr>
+								<td class="full-width-content-wrapper-bottom"><p class="back-top"><a href="#top"><span>Volver Arriba</span></a></p></td>
+							</tr>
+						</table>
+					</div><!-- END .full-width-wrapper -->
+				</div><!-- END .content -->
+			</div><!-- END .content-wrapper -->
+			<div class="clear-footer"></div>
+		</div><!-- END .container -->
+		<?php include('_components/footer.html'); ?>
+	</body>
+</html>
