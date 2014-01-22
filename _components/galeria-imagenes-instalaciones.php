91b13ab7873c90c34c5c3650c77a374f99600109
Daniel González Briseño <sonusbeat@hotmail.com>
2014-01-21 18:32:13 -0600

    Se agregaron imagenes tamaño completo de instalaciones

--- _components/galeria-imagenes-instalaciones.php
+++ _components/galeria-imagenes-instalaciones.php
@@ -2,58 +2,58 @@
 	<div class="index-list">
 		<div class="row">
 			<div class="index-item">
-				<a href="#"><img src="/images/instalaciones/miniaturas/entrada-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=entrada-restaurant" title="Ver Imagen Tamaño Completo"title="Ver Imagen Tamaño Completo"><img src="/images/instalaciones/miniaturas/entrada-zazon-restaurant-bar-135x135.jpg" alt="Entrada Zazón Restaurant Bar" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/buffete-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=barra-ensaladas-buffete" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/buffete-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/vinos-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=vinos" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/vinos-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/interior-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=interior-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/interior-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 		</div><!-- END .row -->
 		<div class="row">
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/interior-clientes-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=interior-clientes-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/interior-clientes-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/seccion-interior-terrazas-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=seccion-interior-terrazas-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/seccion-interior-terrazas-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/interior-buffete-lateral-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=interior-buffete-lateral-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/interior-buffete-lateral-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/cantante-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=cantante-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/cantante-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 		</div><!-- END .row -->
-		<div class="row">
+		<div class="row" style="background: none;">
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/interior-terrazas-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=interior-terrazas-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/interior-terrazas-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/terraza-guadalupe-tacos-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=terraza-guadalupe-tacos-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/terraza-guadalupe-tacos-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/terraza-guadalupe-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=terraza-guadalupe-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/terraza-guadalupe-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 			<div class="index-item">
-				<a href="#"><img src="images/instalaciones/miniaturas/interior-vista-lateral-buffete-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
+				<a href="imagen-instalacion.php?imagen-instalacion=interior-vista-lateral-buffete-zazon-restaurant-bar" title="Ver Imagen Tamaño Completo"><img src="images/instalaciones/miniaturas/interior-vista-lateral-buffete-zazon-restaurant-bar-135x135.jpg" alt="" width="135" height="135" /></a>
 			</div><!-- END .index-item -->
 		</div><!-- END .row -->
-		<div class="pages">
+		<!-- <div class="pages">
 			<table>
 				<tr>
 					<td>
-						<!-- <a href="#" class="previous"><span>Anterior</span></a> -->
-						<a href="#" class="active"><span>1</span></a>
-						<a href="#"><span>2</span></a>
-						<a href="#" class="next"><span>Siguientes</span></a>
+						<a href="imagen-instalacion.php?imagen-instalacion=placeholder" class="previous"><span>Anterior</span></a>
+						<a href="imagen-instalacion.php?imagen-instalacion=placeholder" class="active"><span>1</span></a>
+						<a href="imagen-instalacion.php?imagen-instalacion=placeholder"><span>2</span></a>
+						<a href="imagen-instalacion.php?imagen-instalacion=placeholder" class="next"><span>Siguientes</span></a>
 					</td>
 				</tr>
 			</table>
-		</div><!-- END .pages -->
+		</div> --><!-- END .pages -->
 	</div><!-- END .index-list -->
 </div><!-- END .photo-gallery -->
 <div class="clear"></div>
---
+++ includes/instalaciones_array-dev.php
@@ -0,0 +1,66 @@
+<?php
+
+$instalaciones = array(
+	'entrada-restaurant' => array(
+		'titulo' => 'Entrada Zazón Restaurant Bar',
+		'img' => 'entrada-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'barra-ensaladas-buffete' => array(
+		'titulo' => 'Barra de Ensaladas y Buffete',
+		'img' => 'buffete-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'vinos' => array(
+		'titulo' => 'Vinos',
+		'img' => 'vinos-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'interior-zazon-restaurant-bar' => array(
+		'titulo' => 'Interior Zazón Restaurant Bar',
+		'img' => 'interior-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'interior-clientes-zazon-restaurant-bar' => array(
+		'titulo' => 'Interior Zazón Restaurant Bar',
+		'img' => 'interior-clientes-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'seccion-interior-terrazas-zazon-restaurant-bar' => array(
+		'titulo' => 'Interior Zazón Restaurant Bar',
+		'img' => 'seccion-interior-terrazas-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'interior-buffete-lateral-zazon-restaurant-bar' => array(
+		'titulo' => 'Interior Zazón Restaurant Bar',
+		'img' => 'interior-buffete-lateral-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'cantante-zazon-restaurant-bar' => array(
+		'titulo' => 'Cantante con Grupo en Vivo',
+		'img' => 'cantante-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'interior-terrazas-zazon-restaurant-bar' => array(
+		'titulo' => 'Interior Zazón Restaurant Bar',
+		'img' => 'interior-terrazas-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'terraza-guadalupe-tacos-zazon-restaurant-bar' => array(
+		'titulo' => 'Cocinera en Terraza',
+		'img' => 'terraza-guadalupe-tacos-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'terraza-guadalupe-zazon-restaurant-bar' => array(
+		'titulo' => 'Terraza lado Avenida Guadalupe',
+		'img' => 'terraza-guadalupe-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),
+	'interior-vista-lateral-buffete-zazon-restaurant-bar' => array(
+		'titulo' => 'Interior Zazón Restaurant Bar',
+		'img' => 'interior-vista-lateral-buffete-zazon-restaurant-bar.jpg',
+		'back-link' => 'instalaciones-zazon-restaurant-bar'
+	),	
+);
+
+?>
